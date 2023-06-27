c737368dabf0c396c1c17175a79390c621acfde9
ikenaga530 <takumi.ne.jp530@gmail.com>
2023-06-28 00:46:29 +0900

    add discord icon

--- discord/src/components/sidebar/Sidebar.tsx
+++ discord/src/components/sidebar/Sidebar.tsx
@@ -14,10 +14,10 @@ function Sidebar() {
       {/* sidebarLeft */}
       <div className="sidebarLeft">
         <div className="serverIcon">
-          <img src="./logo192.png" alt="" />
+          <img src="./discordicon.png" alt="" />
         </div>
         <div className="serverIcon">
-          <img src="./logo192.png" alt="" />
+          <img src="./discordicon.png" alt="" />
         </div>
       </div>
       {/* sidebarRight */}
