8f77a89f238ea5688615f492d9862c36119a2fa9
ikenaga530 <takumi.ne.jp530@gmail.com>
2023-07-16 23:10:35 +0900

    init recoil

--- discord/src/components/chat/Chat.tsx
+++ discord/src/components/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Chat.scss";
 import ChatHeader from "./ChatHeader";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
@@ -7,9 +7,39 @@ import GifIcon from "@mui/icons-material/Gif";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 import ChatMessage from "./ChatMessage";
 import { useAppSelector } from "../../app/hokks";
+import {
+  CollectionReference,
+  DocumentData,
+  addDoc,
+  collection,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../../firebase";
 
 function Chat() {
+  const [inputText, setInputText] = useState<string>("");
   const channelName = useAppSelector((state) => state.channel.channelName);
+  const channelId = useAppSelector((state) => state.channel.channelId);
+  const user = useAppSelector((state) => state.user.user);
+  const sendMessage = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    //channelsコレクションの中にあるmessagesコレクションの中にメッセージ情報を入れる
+    const collentionRef: CollectionReference<DocumentData> = collection(
+      db,
+      "channels",
+      String(channelId),
+      "messages"
+    );
+
+    await addDoc(collentionRef, {
+      message: inputText,
+      timestamp: serverTimestamp(),
+      user: user,
+    });
+  };
 
   return (
     <div className="chat">
@@ -25,8 +55,20 @@ function Chat() {
       <div className="chatInput">
         <AddCircleOutlineIcon />
         <form>
-          <input type="text" placeholder="#Udemyへメッセージを送信" />
-          <button type="submit" className="chatInputButton">
+          <input
+            type="text"
+            placeholder="#Udemyへメッセージを送信"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setInputText(e.target.value)
+            }
+          />
+          <button
+            type="submit"
+            className="chatInputButton"
+            onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+              sendMessage(e)
+            }
+          >
             送信
           </button>
         </form>
--- todo-ts/src/App.tsx
+++ todo-ts/src/App.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [inputValue, setInputValue] = useState("")
+  const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
   type Todo = {
     inputValue: string;
     id: number;
     checked: boolean;
-  }
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value)
-  }
+    setInputValue(e.target.value);
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -27,55 +27,69 @@ function App() {
     };
 
     setTodos([newTodo, ...todos]);
-    setInputValue("")
+    setInputValue("");
   };
 
   const handleEdit = (id: number, inputValue: string) => {
     const newTodos = todos.map((todo) => {
-      if(todo.id == id){
+      if (todo.id == id) {
         todo.inputValue = inputValue;
       }
       return todo;
     });
-      setTodos(newTodos);
+    setTodos(newTodos);
   };
 
   const handleChecked = (id: number, checked: boolean) => {
     const newTodos = todos.map((todo) => {
-      if(todo.id == id){
+      if (todo.id == id) {
         todo.checked = !checked;
       }
       return todo;
     });
-      setTodos(newTodos);
+    setTodos(newTodos);
   };
 
-  const handleDelete = (id: number) =>{
+  const handleDelete = (id: number) => {
     const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
-  }
+  };
 
   return (
     <div className="App">
       <div>
         <h2>Todoリスト with Typescript</h2>
-        <form onSubmit={(e) => {handleSubmit(e)}}>
-          <input type="text" onChange={(e) => {handleChange(e)}} className="inputText"/>
-          <input type="submit" value="作成"　className="submitButton" />
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          <input
+            type="text"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            className="inputText"
+          />
+          <input type="submit" value="作成" className="submitButton" />
         </form>
         <ul className="todoList">
           {todos.map((todo) => (
             <li key={todo.id}>
               <input
                 type="text"
-                onChange={(e) => {handleEdit(todo.id, e.target.value)}}
+                onChange={(e) => {
+                  handleEdit(todo.id, e.target.value);
+                }}
                 className="inputText"
                 value={todo.inputValue}
                 disabled={todo.checked}
               />
               <input
                 type="checkbox"
-                onChange={(e) => {handleChecked(todo.id, todo.checked)}}
+                onChange={(e) => {
+                  handleChecked(todo.id, todo.checked);
+                }}
               />
               <button onClick={() => handleDelete(todo.id)}>消</button>
             </li>
