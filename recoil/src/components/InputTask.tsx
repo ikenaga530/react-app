566eff1c8827160af31039cd241bf111840cb7fd
ikenaga530 <takumi.ne.jp530@gmail.com>
2023-07-16 23:58:26 +0900

    [add] recoil

---
+++ recoil/src/components/InputTask.tsx
@@ -0,0 +1,30 @@
+import React, { useCallback } from "react";
+import "./InputTask.css";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { inputTitleState } from "../states/inputTitleState";
+
+const InputTask = () => {
+  const inputTitle = useRecoilValue(inputTitleState);
+  const setInputTitle = useSetRecoilState(inputTitleState);
+  const handleClick = () => {
+    console.log(inputTitle);
+  };
+
+  const onChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputTitle(e.target.value);
+      console.log(inputTitle);
+    },
+    [inputTitle]
+  );
+  return (
+    <div className="inputField">
+      <input type="text" className="inputTitle" onChange={onChange} />
+      <button type="button" className="addButton" onClick={handleClick}>
+        追加
+      </button>
+    </div>
+  );
+};
+
+export default InputTask;
